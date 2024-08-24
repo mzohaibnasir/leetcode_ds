673977d86c76f3742404664c62815197ebfc1b70
Zohaib <m.zohaibnasir6@gmail.com>
2024-08-24 23:59:00 +0500

    c

---
+++ advanced_c/hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void){
+
+    char s[100];
+
+    printf("Enter the first string: ");
+    scanf("%s",s);
+    printf("Hello %s \n",s);
+}
+
+
+
+// make hello
+// ./hello
+
+// \n quotation marks
